61a61d7faa755e03acee7d1314f4bfa66fa833c8
Shishir Aryal <67183366+csr88@users.noreply.github.com>
2021-05-08 09:44:15 -0400

    Create Phase3_ServerWorker.java

---
+++ Phase3_ServerWorker.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class ServerWorker extends  Thread {
+
+    private final Socket clientSocket;
+
+    public ServerWorker(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSocket(clientSocket);
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i=0;i<10;i++){
+            outputStream.write(("Datetime is " + new Date() + "\n").getBytes(StandardCharsets.UTF_8));
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
+
+    }
+}
