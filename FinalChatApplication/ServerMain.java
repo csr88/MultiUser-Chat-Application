769ed92bd8526cfe2bc4850c81d06ba7680e60a7
Shishir Aryal <67183366+csr88@users.noreply.github.com>
2021-05-11 11:44:16 -0400

    Create ServerMain.java

---
+++ FinalChatApplication/ServerMain.java
@@ -0,0 +1,15 @@
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        Server server = new Server(port);
+        server.start();
+    }
+}
+
+
+/* usernames and password
+shishir = p@ssword
+guest = summer2021
+anonymous = mrrobot
+ */
