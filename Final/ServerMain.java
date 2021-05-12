21b4cc7be933f086a0e3b1291bb08ec9b0d213a1
Shishir Aryal <67183366+csr88@users.noreply.github.com>
2021-05-12 06:50:29 -0400

    Create ServerMain.java

---
+++ Final/ServerMain.java
@@ -0,0 +1,15 @@
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        Server server = new Server(port);
+        server.start();
+    }
+}
+
+
+/* usernames and password
+shishir = p@ssword
+guest = guest69
+anonymous = mrrobot
+ */
